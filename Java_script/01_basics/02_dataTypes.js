cbccb51460f086727fe3b29b72b3902327dcd8f0
LOKESHGOPAL21 <125382921+LOKESHGOPAL21@users.noreply.github.com>
2025-07-18 14:58:02 +0000

    all about the dataTypes of JS

---
+++ Java_script/01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(2 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("lokesh")
+
+
+let name = "lokesh gopal"
+let age = 21
+let isLoggedIn = true
+let state;
+
+// number => 2 to poer of 53
+// bigint 
+// string => ""
+//boolean => true/false
+// null => standard alone value
+// undefined = >
+// symbol => unique 
+
+// object
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
+
