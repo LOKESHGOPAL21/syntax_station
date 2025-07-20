7358be501e2b93dfcbe5b79423ec5f194b25e37b
LOKESHGOPAL21 <lokeshgopal2104@gmail.com>
2025-07-20 23:01:26 +0530

    Implemented different data type conversions

---
+++ Java_script/01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let name = "Lokesh"
+
+// console.log(typeof name);
+// console.log(typeof(name));
+
+let score = "33abc"
+// console.log(score)
+// console.log(typeof score)
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Lokesh"
+// console.log(isLoggedIn)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1=>true; 0 =>false
+// ""=> false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
